c7f45ae997f216c650afe8f5daea7fe9621d3c03
Shox Adomokai <adomokai_peter@yahoo.ca>
2020-08-25 14:44:31 +0100

    fix: animation fixes and layout adjustments

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,7 +1,4 @@
 $(function () {
-  let width = 125,
-    height = 125;
-
   let circles = [
     {
       target: ".onboarding-circle",
@@ -62,15 +59,82 @@ $(function () {
   ];
 
   //ANIMATIONS START HERE
+  const circleAnimation = () => {
+    for (let [index, circle] of circles.entries()) {
+      let tl = anime.timeline({
+        easing: "easeOutExpo",
+        targets: circle.target,
+        duration: 2000
+      });
+      tl.add({
+        top: circle.top,
+        left: circle.left,
+        backgroundColor: circle.backgroundColor,
+        width: 125,
+        height: 125,
+        delay: 1000 + index * 200
+      }).add(
+        {
+          targets: circle.image,
+          scale: [0, 1],
+          opacity: 1,
+          duration: 1000,
+          complete: function (el) {
+            document.querySelector(circle.target).classList.add("pulse");
+            if (index === circles.length - 1) {
+              squareAnimation2();
+            }
+          }
+        },
+        "-=700"
+      );
+    }
+  };
 
-  let squareAnimation = anime.timeline({
-    duration: 1000,
-    easing: "easeOutExpo",
-    delay: 100
-  });
+  const squareAnimation2 = () => {
+    let tl = anime.timeline({
+      easing: "easeOutExpo",
+      duration: 1000
+    });
 
-  squareAnimation
-    .add({
+    tl.add({
+      targets: ".center-square",
+      easing: "easeOutExpo",
+      width: 181,
+      height: 181
+    }).add(
+      {
+        targets: ".center-square .images",
+        opacity: [0, 1],
+        duration: 2000,
+        begin: imageHover(),
+        complete: () => {
+          document.querySelector(".circles").classList.add("rotate");
+        }
+      },
+      "-=500"
+    );
+  };
+
+  const imageHover = () => {
+    let images = document.querySelectorAll(".center-square .images img");
+
+    new hoverEffect({
+      parent: document.querySelector(".center-square .images"),
+      intensity: 0.3,
+      image1: images[0].getAttribute("src"),
+      image2: images[1].getAttribute("src"),
+      displacementImage: "./assets/js/distortion.png"
+    });
+  };
+
+  const squareAnimation = () => {
+    let tl = anime.timeline({
+      duration: 1000,
+      easing: "easeOutExpo"
+    });
+
+    tl.add({
       targets: [
         ".blue-square",
         ".purple-square",
@@ -78,91 +142,41 @@ $(function () {
         ".center-square"
       ],
       opacity: [0, 1],
-      delay: anime.stagger(100),
+      delay: anime.stagger(100, { start: 1100 }),
       translateY: ["50px", "0"]
     })
-    .add({
-      targets: ".blue-square",
-      left: ["0", "31.61%"],
-      top: ["65.86%", "55.95%"],
-      width: 40.46,
-      height: 40.46,
-      opacity: 0
-    })
-    .add(
-      {
-        targets: ".purple-square",
-        left: "61.43%",
-        top: "31.72%",
-        width: 38.99,
-        height: 38.99,
+      .add({
+        targets: ".blue-square",
+        left: ["0", "31.61%"],
+        top: ["65.86%", "55.95%"],
+        width: 40.46,
+        height: 40.46,
         opacity: 0
-      },
-      "-=1000"
-    )
-    .add(
-      {
-        targets: ".green-square",
-        left: ["82.68%", "62.02%"],
-        top: ["83.33%", "62.38%"],
-        width: 22.81,
-        height: 22.81,
-        opacity: 0
-      },
-      "-=1000"
-    );
+      })
+      .add(
+        {
+          targets: ".purple-square",
+          left: "61.43%",
+          top: "31.72%",
+          width: 38.99,
+          height: 38.99,
+          opacity: 0
+        },
+        "-=1000"
+      )
+      .add(
+        {
+          targets: ".green-square",
+          left: ["82.68%", "62.02%"],
+          top: ["83.33%", "62.38%"],
+          width: 22.81,
+          height: 22.81,
+          opacity: 0,
+          complete: circleAnimation()
+        },
+        "-=1000"
+      );
+  };
 
-  for (let [index, circle] of circles.entries()) {
-    let tl = anime.timeline({
-      easing: "easeOutExpo",
-      targets: circle.target,
-      duration: 1000
-    });
-
-    tl.add({
-      top: circle.top,
-      left: circle.left,
-      backgroundColor: circle.backgroundColor,
-      width,
-      height,
-      delay: 1000 + index * 50
-    }).add(
-      {
-        targets: circle.image,
-        scale: [0, 1],
-        opacity: 1
-      },
-      "-=700"
-    );
-  }
-
-  anime({
-    targets: ".center-square",
-    duration: 1500,
-    easing: "easeOutExpo",
-    delay: 1000,
-    width: 181,
-    height: 181
-  });
-
-  anime({
-    targets: ".center-square .images",
-    duration: 1800,
-    opacity: [0, 1],
-    scale: [0, 1],
-    easing: "easeOutExpo",
-    delay: 1000
-  });
-
-  let images = document.querySelectorAll(".center-square .images img");
-
-  setTimeout(() => {
-    var imageHover = new hoverEffect({
-      parent: document.querySelector(".center-square .images"),
-      intensity: 0.3,
-      image1: images[0].getAttribute("src"),
-      image2: images[1].getAttribute("src"),
-      displacementImage: "./assets/js/4.png"
-    });
-  }, 1000);
+  if (window.innerWidth >= 1200) squareAnimation();
 });
